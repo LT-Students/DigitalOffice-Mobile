bbfa59eec4b852d384e8fffc9670bcaf94599396
Spartak Ryabtsev <spartak.ryabtsev@lanit-tercom.com>
2020-10-07 20:40:50 +0300

    Add draft projects

---
+++ src/LT.DigitalOffice.Mobile/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using LT.DigitalOffice.Mobile.Services;
+using LT.DigitalOffice.Mobile.Views;
+
+namespace LT.DigitalOffice.Mobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ src/LT.DigitalOffice.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using LT.DigitalOffice.Mobile.ViewModels;
+
+namespace LT.DigitalOffice.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
